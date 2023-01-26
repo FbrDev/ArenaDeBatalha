0cc5ad14932094e019d2745a3550d2f637edfa5c
fpcmaria@apoioprodesp.sp.gov.br <fpcmaria@apoioprodesp.sp.gov.br>
2023-01-26 13:33:05 -0300

    Objeto Background a tela de fundo do jogo. Lógica de renderização dos objetos no formulário

--- ArenaDeBatalha.GUI/FormPrincipal.Designer.cs
+++ ArenaDeBatalha.GUI/FormPrincipal.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.SuspendLayout();
             // 
             // FormPrincipal
@@ -35,8 +36,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arena de Batalha";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPrincipal_Paint);
             this.ResumeLayout(false);
 
         }
--- ArenaDeBatalha.GUI/FormPrincipal.cs
+++ ArenaDeBatalha.GUI/FormPrincipal.cs
@@ -1,20 +1,58 @@
-﻿using System;
+﻿using ArenaDeBatalha.GameLogic;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace ArenaDeBatalha.GUI
 {
     public partial class FormPrincipal : Form
     {
+        DispatcherTimer gameLoopTimer { get; set; }
+        Bitmap screenBuffer { get; set; }
+        Graphics screenPainter { get; set; }
+        Background background { get; set; }
+        List<GameObject> gameObjects { get; set; }
+
         public FormPrincipal()
         {
             InitializeComponent();
+
+            this.ClientSize = Media.fundo.Size;
+            this.screenBuffer = new Bitmap(Media.fundo.Width, Media.fundo.Height);
+            this.screenPainter = Graphics.FromImage(this.screenBuffer);
+            this.gameObjects = new List<GameObject>();
+            this.background = new Background(this.screenBuffer.Size, this.screenPainter);
+
+            this.gameLoopTimer = new DispatcherTimer(DispatcherPriority.Render);
+            this.gameLoopTimer.Interval = TimeSpan.FromMilliseconds(16.66666);
+            this.gameLoopTimer.Tick += GameLoop;
+
+            this.gameObjects.Add(this.background);
+
+            StartGame();
+        }
+
+        public void StartGame()
+        {
+            this.gameLoopTimer.Start();
+        }
+
+        public void GameLoop(object sender, EventArgs e)
+        {
+            foreach(GameObject go in this.gameObjects)
+            {
+                go.UpdateObject();
+                this.Invalidate();
+            }
+        }
+
+        private void FormPrincipal_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.DrawImage(this.screenBuffer, 0, 0);
         }
     }
 }
