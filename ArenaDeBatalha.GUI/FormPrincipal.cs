c1ea420933360f647f3e7d87f51abf80f439f8f2
fpcmaria@apoioprodesp.sp.gov.br <fpcmaria@apoioprodesp.sp.gov.br>
2023-01-28 21:04:18 -0300

    Criando objeto Enemy adicionando método que cria 1 inimigo na tela a cada segundo.

--- ArenaDeBatalha.GUI/FormPrincipal.cs
+++ ArenaDeBatalha.GUI/FormPrincipal.cs
@@ -11,15 +11,18 @@ namespace ArenaDeBatalha.GUI
     public partial class FormPrincipal : Form
     {
         DispatcherTimer gameLoopTimer { get; set; }
+        DispatcherTimer enemySpawnTimer { get; set; }
         Bitmap screenBuffer { get; set; }
         Graphics screenPainter { get; set; }
         Background background { get; set; }
         List<GameObject> gameObjects { get; set; }
+        public Random random { get; set; }
 
         public FormPrincipal()
         {
             InitializeComponent();
-
+            
+            this.random = new Random();
             this.ClientSize = Media.fundo.Size;
             this.screenBuffer = new Bitmap(Media.fundo.Width, Media.fundo.Height);
             this.screenPainter = Graphics.FromImage(this.screenBuffer);
@@ -30,6 +33,10 @@ namespace ArenaDeBatalha.GUI
             this.gameLoopTimer.Interval = TimeSpan.FromMilliseconds(16.66666);
             this.gameLoopTimer.Tick += GameLoop;
 
+            this.enemySpawnTimer = new DispatcherTimer(DispatcherPriority.Render);
+            this.enemySpawnTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            this.enemySpawnTimer.Tick += SpawnEnemy;
+
             this.gameObjects.Add(this.background);
 
             StartGame();
@@ -38,13 +45,29 @@ namespace ArenaDeBatalha.GUI
         public void StartGame()
         {
             this.gameLoopTimer.Start();
+            this.enemySpawnTimer.Start();
+        }
+
+        public void SpawnEnemy(object sender, EventArgs e)
+        {
+            Point enemyPosition = new Point(this.random.Next(10, this.screenBuffer.Width - 74), -62);
+            Enemy enemy = new Enemy(this.screenBuffer.Size, this.screenPainter, enemyPosition); 
+            this.gameObjects.Add(enemy);
         }
 
         public void GameLoop(object sender, EventArgs e)
         {
+            this.gameObjects.RemoveAll(x => !x.Active);
+
             foreach(GameObject go in this.gameObjects)
             {
                 go.UpdateObject();
+
+                if (go.IsOutOfBounds())
+                {
+                    go.Destroy();
+                }
+
                 this.Invalidate();
             }
         }
